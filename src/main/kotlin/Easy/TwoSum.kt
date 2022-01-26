0ddfe7be23b101a74a530654df0449061c5404e7
alexeypetrov <lomon2008@yandex.ru>
2022-01-26 22:42:46 +0300

    TwoSum

---
+++ src/main/kotlin/Easy/TwoSum.kt
@@ -0,0 +1,14 @@
+package Easy
+
+class TwoSum {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        val indexesMap=HashMap<Int, Int>()
+        nums.forEachIndexed{index, num->
+            if(indexesMap.get(num)!=null){
+                return intArrayOf(indexesMap.get(num)!!,index)
+            }
+            indexesMap.put(target-num, index)
+        }
+        throw RuntimeException()
+    }
+}
