e23776ed41112d9c2b33c46453ded6100dab4d4a
alexeypetrov <lomon2008@yandex.ru>
2022-07-25 08:19:27 +0300

    AddTwoNumbers.kt

---
+++ src/main/kotlin/medium/AddTwoNumbers.kt
@@ -0,0 +1,16 @@
+package medium
+
+class AddTwoNumbers {
+    fun addTwoNumbers(l1: ListNode?, l2: ListNode?): ListNode? {
+        var shouldGoNext = true
+        var result = ListNode(0)
+        while (shouldGoNext) {
+
+        }
+        return ListNode(0)
+    }
+
+    class ListNode(var value: Int) {
+        var next: ListNode? = null
+    }
+}
